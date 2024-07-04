913291828111de0643f78a5999bbd8990b50451b
chasyuss <cotn963@gmail.com>
2024-07-04 16:58:23 +0900

    fix: 이미지와 카드 크기 수정

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Country } from '../types/Country';
+import React from "react";
+import { Country } from "../types/Country";
 
 interface CountryCard {
   country: Country;
@@ -7,15 +7,25 @@ interface CountryCard {
   onClick: () => void;
 }
 
-const CountryCard: React.FC<CountryCard> = ({ country, isSelected, onClick}) => {
+const CountryCard: React.FC<CountryCard> = ({
+  country,
+  isSelected,
+  onClick,
+}) => {
   return (
     <div
-    className={`p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-transform cursor-pointer ${isSelected ? 'border-4 border-green-500' : ''}`}
-    onClick={onClick}
-  >
-      <img src={country.flags.png} alt={`Flag of ${country.name.common}`} className='w-20 h-auto mx-auto mb-4'/>
-      <h2 className='text-xl font-bold mb-2'>{country.name.common}</h2>
-      <p className='text-gray-600'> {country.capital}</p>
+      className={`p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-transform cursor-pointer h-40 border ${
+        isSelected ? "border-4 border-green-500" : ""
+      }`}
+      onClick={onClick}
+    >
+      <img
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        className="w-8 h-8 mx-auto mb-4 object-contain"
+      />
+      <h2 className="text-xl font-bold mb-2">{country.name.common}</h2>
+      <p className="text-gray-600"> {country.capital}</p>
     </div>
   );
 };
